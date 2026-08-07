d84562ccbf754b6fbbe54f8acff0b814cc77356a
Harsh Raj <hello@harshraj.com>
2026-08-07 15:33:14 +0530

    fix: disable HUD pointer-events on mobile to allow scroll and revert touchAction

--- src/components/PortfolioScene.tsx
+++ src/components/PortfolioScene.tsx
@@ -536,7 +536,7 @@ export default function PortfolioScene() {
 
       {/* --- 3D CINEMATIC WEBGL CANVAS --- */}
       <Canvas
-        style={{ touchAction: "auto" }}
+        style={{ touchAction: "none" }}
         camera={{ position: [0, 2, 15], fov: 45, near: 0.1, far: 800 }}
         gl={{ 
           antialias: qualitySettings.antialias, 
